bb2de911f5a3603d8bec29dac1473786a412a2ef
Gem Akinbo <127473139+G24-gem@users.noreply.github.com>
2026-06-30 09:20:50 +0100

    Remove specific CORS origin configuration

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors());
 app.use(express.json());
 
 // Health check
